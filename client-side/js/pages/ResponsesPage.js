13a624e7adc6149108198f348a7a2e3013ea4e1d
zacharytabler <zachary.tabler@gmail.com>
2021-08-10 21:17:51 -0400

    Responses page formatting

--- client-side/js/pages/ResponsesPage.js
+++ client-side/js/pages/ResponsesPage.js
@@ -5,12 +5,16 @@ export default function ResponsesPage(responses) {
     .map((response) => {
       return `
     <div class="user_response">
-    <p class="mood_response">We hear you! You feel ${response.mood.mood},</p>
-    <input type='hidden' id='moodId' value='${response.mood.id}'>
-    <p class="trigger_response"> because of ${response.trigger.name},</p>
-    <input type='hidden' id='triggerId' value='${response.trigger.id}'>
-    <p class="copingMechanism_response">which causes you to ${response.copingMechanism.title}.</p>
-    <input type='hidden' id='copingMechanismId' value='${response.copingMechanism.id}'>
+    <span class="mood_response">We hear you! You feel ${response.mood.mood}</span>
+    <input type='hidden' id='moodId' value=${response.mood.id}>
+    <span class="trigger_response"> because of ${response.trigger.name}</span>
+    <input type='hidden' id='triggerId' value=${response.trigger.id}>
+    <span class="copingMechanism_response">which causes you to ${response.copingMechanism.title}.</span>
+    <input type='hidden' id='copingMechanismId' value=${response.copingMechanism.id}>
+   </div>
+   <div>
+    <p class="closing_para">Here are a list of resources to assist you.</p>
+    </div>
       `;
     })
     .join("")}
