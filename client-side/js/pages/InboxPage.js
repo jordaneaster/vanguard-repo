84c8914eb9bcf914d93cb16117960aaf5d962eb1
ChelsMarea1 <chelsea.james0@yahoo.com>
2021-08-20 18:31:59 -0400

    inital commit

--- client-side/js/pages/InboxPage.js
+++ client-side/js/pages/InboxPage.js
@@ -1,10 +1,13 @@
+import TopImg from '../../images/Message_img.png'
+
+
 export default function InboxPage(reply, newReply) {
   return `
+  <img class="message__top" src="${TopImg}">
   <div class='myInbox'>
   ${reply
     .map((replies) => {
       return `
-      <h1 class="myId">Healthy Response</h1>
       <div class="inbox">
       <p class="subject">${replies.subject}</p>
       <p class="title">${replies.title}</p> 
